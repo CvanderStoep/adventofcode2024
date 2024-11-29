8189e58d1747cedceb1e38195d8828179fa1058e
Carlo van der Stoep <carlo.vanderstoep@gmail.com>
2024-11-29 20:07:55 +0100

    Initial commit

---
+++ day0.py
@@ -0,0 +1,23 @@
+def read_input_file(file_name: str) -> list:
+    with open(file_name) as f:
+        content = f.read().splitlines()
+
+    content = list(map(int, content))
+
+    return content
+
+
+def compute_part_one(file_name: str) -> int:
+    inputs = read_input_file(file_name)
+    print(inputs)
+    return 1
+
+
+def compute_part_two(file_name: str) -> int:
+    inputs = read_input_file(file_name)
+    return 2
+
+
+if __name__ == '__main__':
+    print(f"Part I: {compute_part_one('test/input/input1.txt')}")
+    print(f"Part II: {compute_part_two('test/input/input1.txt')}")
